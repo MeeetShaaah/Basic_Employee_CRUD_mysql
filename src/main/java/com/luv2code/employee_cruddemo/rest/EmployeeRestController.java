2ebed5fed63b5d2ef534b47deedf4d75b44c0a37
Meet Shah <meetshah7199@gmail.com>
2023-11-21 14:04:33 -0500

    Connect rest to service layer.

--- src/main/java/com/luv2code/employee_cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/employee_cruddemo/rest/EmployeeRestController.java
@@ -6,20 +6,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.luv2code.employee_cruddemo.dao.EmployeeDao;
 import com.luv2code.employee_cruddemo.entity.Employee;
+import com.luv2code.employee_cruddemo.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
-    private EmployeeDao employeeDao;
+    
+    // inject service to the rest so it works into link from database to dao to service to rest...
+    private EmployeeService employeeService;
 
-    public EmployeeRestController(EmployeeDao theEmployeeDao) {
-        this.employeeDao = theEmployeeDao;
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        this.employeeService = theEmployeeService;
     }
     
     @GetMapping("/employees")
-    public List<Employee> getAllEmployees() {
-        return employeeDao.findAll();
+    public List<Employee> findAll() {
+        return employeeService.findAll();
     }
 }
