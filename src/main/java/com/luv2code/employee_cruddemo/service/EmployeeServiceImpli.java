c2727e12c2b12c3a63eeb274e881733e4a4a47d2
Meet Shah <meetshah7199@gmail.com>
2023-11-21 17:32:39 -0500

    Start with Sprind Data Jpa.

--- src/main/java/com/luv2code/employee_cruddemo/service/EmployeeServiceImpli.java
+++ src/main/java/com/luv2code/employee_cruddemo/service/EmployeeServiceImpli.java
@@ -1,11 +1,13 @@
 package com.luv2code.employee_cruddemo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luv2code.employee_cruddemo.dao.EmployeeDao;
+import com.luv2code.employee_cruddemo.dao.EmployeeJpaRepository;
 import com.luv2code.employee_cruddemo.entity.Employee;
 
 import jakarta.transaction.Transactional;
@@ -13,34 +15,73 @@ import jakarta.transaction.Transactional;
 @Service
 public class EmployeeServiceImpli implements EmployeeService{
 
-    private EmployeeDao employeeDao;
+    // private EmployeeDao employeeDao;
+    
+    // @Autowired
+    // public EmployeeServiceImpli(EmployeeDao theEmployeeDao){
+    //     this.employeeDao = theEmployeeDao;
+    // }
+
+    // @Transactional
+    // @Override
+    // public List<Employee> findAll(){
+    //     return employeeDao.findAll();
+    // }
+
+    // @Transactional
+    // @Override
+    // public Employee findById(int theID) {
+    //     return employeeDao.findById(theID);
+    // }
+
+    // @Transactional
+    // @Override
+    // public Employee save(Employee theEmployee) {
+    //    return employeeDao.save(theEmployee);
+    // }
+    
+    // @Transactional
+    // @Override
+    // public void deletebyId(int theId) {
+    //     employeeDao.deletebyId(theId);
+    // } 
+
+
+    // Moving from JPA Api to Spring Data JPA.....For that, change the EmployeeDao to EmployeeJpaRepository and employeeDao to employeeJpaREpository
+    
+    private EmployeeJpaRepository employeeJpaRepository;
     
     @Autowired
-    public EmployeeServiceImpli(EmployeeDao theEmployeeDao){
-        this.employeeDao = theEmployeeDao;
+    public EmployeeServiceImpli(EmployeeJpaRepository theEmployeeJpaRepository){
+        this.employeeJpaRepository = theEmployeeJpaRepository;
     }
 
-    @Transactional
     @Override
     public List<Employee> findAll(){
-        return employeeDao.findAll();
+        return employeeJpaRepository.findAll();
     }
 
-    @Transactional
     @Override
     public Employee findById(int theID) {
-        return employeeDao.findById(theID);
+        Optional<Employee> resultOptional = employeeJpaRepository.findById(theID);
+        
+        Employee theEmployee = null;
+        if (resultOptional.isPresent()){
+            theEmployee = resultOptional.get();
+        }else{
+            throw new RuntimeException("Did not find employee id: " + theID);
+        }
+
+        return theEmployee;
     }
 
-    @Transactional
     @Override
     public Employee save(Employee theEmployee) {
-       return employeeDao.save(theEmployee);
+       return employeeJpaRepository.save(theEmployee);
     }
     
-    @Transactional
-    @Override
-    public void deletebyId(int theId) {
-        employeeDao.deletebyId(theId);
-    } 
+    public void deletebyId(int theId){
+        employeeJpaRepository.deleteById(theId);
+    }
+
 }
