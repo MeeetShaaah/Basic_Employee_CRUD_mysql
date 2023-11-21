f4b59d2a69751e1c5eec9e550b90dc4602b67415
Meet Shah <meetshah7199@gmail.com>
2023-11-21 15:08:16 -0500

    Update service layer for now DAO methods

--- src/main/java/com/luv2code/employee_cruddemo/service/EmployeeService.java
+++ src/main/java/com/luv2code/employee_cruddemo/service/EmployeeService.java
@@ -6,4 +6,7 @@ import com.luv2code.employee_cruddemo.entity.Employee;
 
 public interface EmployeeService {
     List<Employee> findAll();
+    Employee findById(int theID);
+    Employee save(Employee theEmployee);
+    void deletebyId(int theId);
 }
