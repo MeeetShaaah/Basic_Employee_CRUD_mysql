57b9c96a8c6e6bc42636a487810ccb36c229c6ba
Meet Shah <meetshah7199@gmail.com>
2023-11-21 15:07:25 -0500

    Update employee DAO for CRUD operation

--- src/main/java/com/luv2code/employee_cruddemo/dao/EmployeeDao.java
+++ src/main/java/com/luv2code/employee_cruddemo/dao/EmployeeDao.java
@@ -6,4 +6,7 @@ import com.luv2code.employee_cruddemo.entity.Employee;
 
 public interface EmployeeDao {
     List<Employee> findAll();
+    Employee findById(int theID);
+    Employee save(Employee theEmployee);
+    void deletebyId(int theId);
 }
