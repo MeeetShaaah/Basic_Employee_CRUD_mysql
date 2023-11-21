21c8c25fb7058954c4d294ab63accfbd17991155
Meet Shah <meetshah7199@gmail.com>
2023-11-21 14:01:07 -0500

    Create service interface.

---
+++ src/main/java/com/luv2code/employee_cruddemo/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.luv2code.employee_cruddemo.service;
+
+import java.util.List;
+
+import com.luv2code.employee_cruddemo.entity.Employee;
+
+public interface EmployeeService {
+    List<Employee> findAll();
+}
